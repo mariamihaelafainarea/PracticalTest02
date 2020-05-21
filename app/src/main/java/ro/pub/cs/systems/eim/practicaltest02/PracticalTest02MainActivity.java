f2a5474876a203870227ac6b0e13cfbaf35bd279
mariamihaelafainarea <mariamihaelafainarea4@gmail.com>
2020-05-21 13:03:23 +0300

    Initial commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+    }
+}
